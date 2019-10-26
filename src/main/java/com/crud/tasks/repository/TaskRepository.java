9071418563fdbd9f7ae082ee55e46815b5c0222c
naros1 <sorankasim@gmail.com>
2019-10-26 02:03:08 +0200

    first commit

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -3,6 +3,7 @@ package com.crud.tasks.repository;
 import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.persistence.Id;
 import java.util.List;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
